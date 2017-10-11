81750d6054758c1c0f287c8ba4df98f5f4ae3854
MonikaPetr <petroniene.m@gmail.com>
2017-10-11 17:57:33 +0300

    v_1

---
+++ JS/index.js
@@ -0,0 +1,54 @@
+var result = 0;
+var action = 0;
+
+window.onload = function() {
+    document.getElementById("result").innerHTML = result;
+};
+
+function clear() {
+    result = 0;
+    document.getElementById("result").innerHTML = result;
+}
+
+function zero() {
+    var zero = 0;
+    action = " " + zero;
+    document.getElementById("result").innerHTML = action;
+}
+
+function one() {
+    var one = 1;
+    action = " " + one
+    document.getElementById("result").innerHTML = action;
+}
+
+function zero() {
+    var zero = 0;
+    document.getElementById("result").innerHTML = zero;
+}
+
+function zero() {
+    var zero = 0;
+    document.getElementById("result").innerHTML = zero;
+}
+
+function zero() {
+    var zero = 0;
+    document.getElementById("result").innerHTML = zero;
+}
+
+function zero() {
+    var zero = 0;
+    document.getElementById("result").innerHTML = zero;
+}
+
+function zero() {
+    var zero = 0;
+    document.getElementById("result").innerHTML = zero;
+}
+
+function zero() {
+    var zero = 0;
+    document.getElementById("result").innerHTML = zero;
+}
+
